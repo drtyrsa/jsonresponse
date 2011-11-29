97d2adebe0c465d7aa8fb8c6c7e14dfb9aba01c7
Vlad Starostin <drtyrsa@yandex.ru>
2011-11-29 22:23:20 +0300

    First commit

---
+++ jsonresponse/response.py
@@ -0,0 +1,9 @@
+﻿# -*- coding:utf-8 -*-
+from django.http import HttpResponse
+from django.utils import simplejson
+
+
+class JSONResponse(HttpResponse):
+    def __init__(self, obj, **kwargs):
+        data = simplejson.dumps(obj)
+        super(JSONResponse, self).__init__(data, mimetype='application/json', **kwargs)
---
+++ setup.py
@@ -0,0 +1,25 @@
+﻿# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "django-jsonresponse",
+    version = "0.1.0",
+    license = "BSD",
+    description = "Simple Django response that encodes data in JSON",
+    author = "Vlad Starostin",
+    author_email = "drtyrsa@yandex.ru",
+    packages = find_packages(),
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 1 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
